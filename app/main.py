f3b2edd7502a14b56158debd0588746603eb8b2f
Krish Modh <rmodh4@gmail.com>
2026-01-22 20:07:22 +0530

    update admin frontend files

--- app/main.py
+++ app/main.py
@@ -20,5 +20,7 @@ UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), '..', 'uploads')
 def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
